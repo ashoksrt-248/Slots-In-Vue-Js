e2f8eb1d6dd14fb488dd7c1b89028ecf2e17feb6
Ashok Amara <ashok.amara@cesneosilica.com>
2024-04-04 18:18:00 +0530

    two more rows added

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,20 @@
       <Card><h2>Card 9</h2><img src="https://picsum.photos/seed/seed4/300/200" alt="Static Random Image 2" width="210" height="200"></Card>
       <Card><h2>Card 10</h2><img src="https://picsum.photos/seed/seed5/300/200" alt="Static Random Image 1" width="210" height="200"></Card>
     </div>
+    <div class="app4">
+      <Card><h2>Card 11</h2><img src="https://picsum.photos/seed/seed10/300/200" width="210" height="200" /></Card>
+      <Card><h2>Card 12</h2><img src="https://picsum.photos/seed/seed9/300/200"  width="210" height="200"/></Card>
+      <Card><h2>Card 13</h2><img src="https://picsum.photos/seed/seed8/300/200"  width="210" height="200"/></Card>
+      <Card><h2>Card 14</h2><img src="https://picsum.photos/seed/seed7/300/200" alt="Static Random Image 2" width="210" height="200"></Card>
+      <Card><h2>Card 15</h2><img src="https://picsum.photos/seed/seed6/300/200" alt="Static Random Image 1" width="210" height="200"></Card>
+    </div>
+    <div class="app5">
+      <Card><h2>Card 16</h2><img src="https://picsum.photos/seed/seed11/300/200" width="210" height="200" /></Card>
+      <Card><h2>Card 17</h2><img src="https://picsum.photos/seed/seed12/300/200"  width="210" height="200"/></Card>
+      <Card><h2>Card 18</h2><img src="https://picsum.photos/seed/seed13/300/200"  width="210" height="200"/></Card>
+      <Card><h2>Card 19</h2><img src="https://picsum.photos/seed/seed14/300/200" alt="Static Random Image 2" width="210" height="200"></Card>
+      <Card><h2>Card 20</h2><img src="https://picsum.photos/seed/seed15/300/200" alt="Static Random Image 1" width="210" height="200"></Card>
+    </div>
   </div>
 </template>
 
@@ -38,6 +52,7 @@ export default {
   margin-top: 10px;
   background-color: silver;
   display: flex;
+  color: azure;
 }
 .app3 {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -48,6 +63,27 @@ export default {
   margin-top: 10px;
   background-color:sandybrown;
   display: flex;
+}
+.app4 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+  background-color:palegreen;
+  display: flex;
+}
+.app5 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+  display: flex;
+  background-color: darkcyan;
+  color: aliceblue;
 }
 .heading{
   background-color: antiquewhite;text-align: center;font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
